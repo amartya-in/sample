f7904ec7a269dda5d3c315d46d7d3894e0dc7641
amar <ayush@Amartyas-MacBook-Pro.local>
2018-07-10 23:46:17 +0800

    First Git

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	std::cout<<"......................................"<<std::endl;
+	std::cout<<"Testing CMake/Stash/Container in Linux"<<std::endl;
+	std::cout<<"......................................"<<std::endl;
+	return 0;
+}
